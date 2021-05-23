d1cc05d1ff06e4f897aa1bb25a4f48d84809a232
bungananda27 <bungatiani@gmail.com>
2021-05-23 09:48:58 +0700

    cek

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', 'RedirectIfAuthenticatedController');
 Auth::routes();
 Route::middleware(['auth','Ceklevel:admin,super,owner,karyawan'])->group(function () {
+    Route::get('/artisan/st0rage', function () {
+        $command = 'storage:link';
+        $result = Artisan::call($command);
+        return Artisan::output();
+
+    });
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('pegawai', 'PegawaiController');
     Route::resource('user', 'UserController');
